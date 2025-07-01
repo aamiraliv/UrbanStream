48d16c58cd83595cb8f73c3e8a6285d67bb4057a
Amir Ali V <amiralivillan@gmail.com>
2025-07-01 10:20:54 +0530

    Landing page (#9)
    * landing page updated
    
    * ui updates
    
    * ui updates

--- Client/UrbanStream/src/App.jsx
+++ Client/UrbanStream/src/App.jsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Intro } from "./page/Intro";
-
+import Home from "./page/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Intro />,
-  }
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
 ]);
-
-
 
 function App() {
   return (
